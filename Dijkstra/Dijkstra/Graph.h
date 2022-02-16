b8ad7a06c33a7698bfd3a30d7788fe5375c55377
Oreyato <oreyato.pro@gmail.com>
2022-02-16 16:10:56 +0100

    refactor: separate Graph in own class

---
+++ Dijkstra/Dijkstra/Graph.h
@@ -0,0 +1,15 @@
+#pragma once
+class Graph
+{
+public:
+	Graph();
+	~Graph();
+
+	// Delete copy constructor & assignment operator
+	Graph(const Graph&) = delete;
+	Graph& operator=(const Graph&) = delete;
+
+private:
+
+};
+
